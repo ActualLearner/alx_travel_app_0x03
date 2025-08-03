5c679c42aa0519fab6054f88efa120080de689d8
ActualLearner <abdellahshafi7@gmail.com>
2025-08-03 09:35:25 +0300

    feat: duplicate alx_travel_app_0x00, create views for Listing & Booking and configure urls

--- alx_travel_app/listings/urls.py
+++ alx_travel_app/listings/urls.py
@@ -1,27 +1,13 @@
 from django.contrib import admin
-from django.urls import path, re_path
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
+from django.urls import path, include
+from rest_framework import routers
+from .views import ListingViewSet, BookingViewSet
 
+router = routers.DefaultRouter()
+router.register(r'listings', ListingViewSet, basename='listing')
+router.register(r'bookings', BookingViewSet, basename='booking')
 
-schema_view = get_schema_view(
-   openapi.Info(
-      title="ALX Travel App API",
-      default_version='v1',
-      description="API documentation for the ALX Travel App, a platform for listing and discovering travel destinations.",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@travelapp.local"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
-)
 
 urlpatterns = [
-    
-    # Swagger UI endpoints
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+   path('', include(router.urls))
 ]
--- alx_travel_app/listings/views.py
+++ alx_travel_app/listings/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import ListingSerializer, BookingSerializer
+from .models import Listing, Booking
 
 # Create your views here.
+
+
+class ListingViewSet(viewsets.ModelViewSet):
+    serializer_class = ListingSerializer
+    queryset = Listing.objects.all()
+    
+    
+class BookingViewSet(viewsets.ModelViewSet):
+    serializer_class = BookingSerializer
+    queryset = Booking.objects.all()
+    
